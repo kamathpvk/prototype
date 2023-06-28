99cd7415eef4e9a9cdd7c10b52bf2cf103ce6b04
kamathpvk <kamathpvk@gmail.com>
2023-06-28 18:32:04 +0530

    edited contact title

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <div name='contact' className=' w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 '>
         <form method='PIST' action='https://getform.io/f/01696395-469b-4f94-b916-a1387fa28e7e' className='flex flex-col max-w-[600px] w-full'>
             <div className=' pb-8 '>
-                <p className=' text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Contact</p>
-                <p className=' text-gray-300 py-4 '>Submit the form below or shoot me an email - email@gmail.com</p>
+                <p className=' text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Let's Connect</p>
+                <p className=' text-gray-300 py-4 '>Submit the form below or shoot me an email at email@gmail.com</p>
             </div>
             <input className=' bg-[#ccd6f6] p-2 ' type='text' placeholder='Name' name='name'/>
             <input className=' my-4 p-2 bg-[#ccd6f6] ' type='email' placeholder='email@gmail.com' name='email'/>
