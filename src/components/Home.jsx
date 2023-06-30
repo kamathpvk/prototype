cfcf53d609f9f908e8a05a09fab086a248257589
kamathpvk <kamathpvk@gmail.com>
2023-06-30 09:08:52 +0530

    edited home description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,8 +13,10 @@ const Home = () => {
             </p>
             <h1 className=' text-4xl sm:text-7xl font-bold text-blue-600 ' >Vibhav Kamath</h1>
             {/* <h2 className=' text-4xl sm:text-7xl font-bold text-gray-300 ' >I'm a Web Developer</h2> */}
-            <p className=' text-gray-400 py-4 max-w-[700px] ' >I'm a full-stack developer specializing in building 
-            exceptional digital experinces. Currently, I'm focused on building responsive full-stack web applications</p>
+            <p className=' text-gray-400 py-4 max-w-[800px] ' >
+              Welcome to my portfolio. Feel free to browse my website, and drop your suggestions on my Github repo. Thank you for visiting!
+              {/* I'm a full-stack developer specializing in building exceptional digital experinces. Currently, I'm focused on building responsive full-stack web applications */}
+            </p>
             <div>
                 <button className=' text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 cursor-default ' >Scroll Down 
                 <span className=' group-hover:rotate-90 duration-300 '>
