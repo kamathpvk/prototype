e39296acbba3adb3cb1b59121db1d75bba22cd85
kamathpvk <kamathpvk@gmail.com>
2023-06-30 09:13:20 +0530

    edited about description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,11 +17,15 @@ const About = () => {
                     <p>Hi. I'm Vibhav, nice to meet you. Please take a look around.</p>
                 </div>
                 <div>
-                    <p>I am passionate about building excellent software that improves
-                    the lives of those around me. I specialize in creating software
-                    for clients ranging from individuals and small-businesses all the
-                    way to large enterprise corporations. What would you do if you had
-                    a software expert available at your fingertips?</p>
+                    <p>I am eager to learn new field of technology, particularly interested in the field of web development.
+                        <br/>
+                        I'm open for development related work and full time job opportunities.
+                        {/* passionate about building excellent software that improves */}
+                        {/* the lives of those around me. I specialize in creating software */}
+                        {/* for clients ranging from individuals and small-businesses all the */}
+                        {/* way to large enterprise corporations. What would you do if you had */}
+                        {/* a software expert available at your fingertips? */}
+                    </p>
                 </div>
             </div>
         </div>
