b168b729459a083449c03490529428cc430a1614
kamathpvk <kamathpvk@gmail.com>
2023-08-16 12:37:49 +0530

    import statement

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ import ReactImg from '../assets/react.png'
 // import Node from '../assets/node.png'
 import FireBase from '../assets/firebase.png'
 // import AWS from '../assets/aws.png'
-// import GitHub from '../assets/github.png'
+import GitHub from '../assets/github.png'
 import Tailwind from '../assets/tailwind.png'
 // import Mongo from '../assets/mongo.png'
 
